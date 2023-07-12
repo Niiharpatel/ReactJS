83c23bc78e00eec34e5c82761ddedca8c8c6aeb6
Nihar Patel <patelnihar14@gmail.com>
2023-07-12 14:13:23 +0530

    Functionalcomponent

--- first_project/FunctionalComp/Mainsec.jsx
+++ first_project/FunctionalComp/Mainsec.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import './Mainsec.css';
 
-import React from 'react'
 
 function Mainsec() {
     return (
         <div className="main">
-            <div className="sidebar">SideBar</div>
-            <div className="maincontent">MainContent</div>
+            <div className="sidebar1">SideBar</div>
+            <div className="maincontent1">MainContent</div>
         </div>
     )
 }
