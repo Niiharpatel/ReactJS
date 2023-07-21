10954bc28e92c86bc9d9ab6424c3b49f4008406c
Nihar Patel <patelnihar14@gmail.com>
2023-07-21 09:35:12 +0530

    ..

--- first_project/src/main.jsx
+++ first_project/src/main.jsx
@@ -1,53 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
 import './index.css'
-import ClassCom from '../ClassComp/ClassCom'
-import Body from '../ClassComp/Body'
-import Footer from '../ClassComp/Footer'
+import App from './App'
 
-import Functional from '../FunctionalComp/Functional'
-import Mainsec from '../FunctionalComp/Mainsec'
-import Footerfun from '../FunctionalComp/Footerfun'
 
-// import UsrState from '../../UsrState';
-
-// import Fontcolorchange from './UseState/Fontcolorchange'
-
-import Fontcolorchange2 from './UseState/Fontcolorchange2'
-import FunProps from './Props/FunProps'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <App /> */}
+    <App />
 
-    {/* <ClassCom />
-
-    <Body />
-
-    <Footer /> */}
-
-
-    {/* <Functional />
-
-    <Mainsec />
-
-    <Footerfun /> */}
-
-    {/* <UsrState /> */}
-
-    {/* <Fontcolorchange /> */}
-
-    {/* <Fontcolorchange2 /> */}
-
-    <FunProps name={"Niihar"} />
-
-    {/* <FunProps obj={{
-      name: "Niihar",
-      age: 23
-    }} /> */}
-
+    
 
 
 
