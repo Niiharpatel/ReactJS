f56b3568309af0b7dc15dcf7cb022249108ea152
Nihar Patel <patelnihar14@gmail.com>
2023-07-24 14:20:40 +0530

    bootstrap card

---
+++ first_project/src/Bootstrap/Bootcom.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Button, Card } from 'react-bootstrap'
+
+function Bootcom() {
+    return (
+        <>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={"https://images.pexels.com/photos/399946/pexels-photo-399946.jpeg?auto=compress&cs=tinysrgb&w=600"} />
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                        Hello user this is card using react bootstrap.
+                    </Card.Text>
+                    <Button variant="primary">Hello!</Button>
+                </Card.Body>
+            </Card>
+        </>
+    )
+}
+
+export default Bootcom
